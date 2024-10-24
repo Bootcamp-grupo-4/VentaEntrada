e93960cb081be9f17c602881feff269163374e1b
gsusag00 <jesus.ag00@gmail.com>
2024-10-24 12:51:16 +0200

    Tests

--- src/main/java/com/capgeticket/ventaEntradas/service/VentaEntradasServiceImpl.java
+++ src/main/java/com/capgeticket/ventaEntradas/service/VentaEntradasServiceImpl.java
@@ -3,24 +3,17 @@ package com.capgeticket.ventaEntradas.service;
 import com.capgeticket.ventaEntradas.dto.BancoDto;
 import com.capgeticket.ventaEntradas.dto.EventoDto;
 import com.capgeticket.ventaEntradas.dto.VentaEntradasDto;
-import com.capgeticket.ventaEntradas.exception.EventoNotFoundException;
 import com.capgeticket.ventaEntradas.feignClients.BancoFeignClient;
 import com.capgeticket.ventaEntradas.model.VentaEntrada;
 import com.capgeticket.ventaEntradas.repository.VentaEntradasRepository;
 import com.capgeticket.ventaEntradas.feignClients.EventoFeignClient;
 import com.capgeticket.ventaEntradas.response.BancoResponse;
 import com.capgeticket.ventaEntradas.response.VentaEntradasResponseDto;
-import feign.FeignException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
-
-import java.time.LocalDateTime;
-import java.util.List;
 
 import static com.capgeticket.ventaEntradas.mapper.EntradaMapper.ventaDtoMapper;
 import static com.capgeticket.ventaEntradas.mapper.EntradaMapper.ventaMapper;
@@ -60,33 +53,14 @@ public class VentaEntradasServiceImpl implements VentaEntradasService{
         //Luego comprobamos que el evento para el que vamos a comprar entrada existe
         ResponseEntity<EventoDto> evento = eventoFeignClient.getEventoById(ventaEntradasDto.getEvento().getId());
 
-        //Comprobar el status code
-        if(evento.getStatusCode() != HttpStatus.OK) {
-            logger.error("Me da a mi que este evento ya no existe");
-            //Si no es OK se tira error
-            throw new EventoNotFoundException(ventaEntradasDto.getEvento().getId());
-        }
-
         if(evento.getBody() == null) {
             //Inventarme nombre de exception
-            throw new IllegalArgumentException("Me da a mi que este evento ya existe");
+            throw new IllegalArgumentException("Hubo un error con la busqueda del evento");
         }
 
         //Enviamos al banco la petición
         BancoDto banco = BancoDto.of(ventaEntradasDto);
-        ResponseEntity<BancoResponse> bancoResponse = null;
-        try{
-            bancoResponse = bancoFeignClient.pay(banco);
-        } catch (FeignException.BadRequest e) {
-
-        }
-        //Tratamos la respuesta recibida
-        if(bancoResponse != null && bancoResponse.getStatusCode() != HttpStatus.OK) {
-            logger.error("Oooops, error en la compra");
-            if(bancoResponse.getBody() != null) {
-                handleBankResponse(bancoResponse.getStatusCode(), bancoResponse.getBody().getError(),bancoResponse.getBody().getMessage());
-            }
-        }
+        ResponseEntity<BancoResponse> bancoResponse = bancoResponse = bancoFeignClient.pay(banco);
         //Si es correcta guardamos
         VentaEntrada venta = ventaMapper(ventaEntradasDto, eventoMapper(evento.getBody()));
         VentaEntradasDto ventaReturn = ventaDtoMapper(venta);
@@ -96,18 +70,9 @@ public class VentaEntradasServiceImpl implements VentaEntradasService{
         return response;
     }
 
-    /**
-     * Valida los valores de compra dados por el usuario
-     * @param ventaEntradasDto
-     */
     private void validarEntrada(VentaEntradasDto ventaEntradasDto) {
-        //tirar exception si esta chunga
+
     }
 
-    private void handleBankResponse(HttpStatusCode statusCode, String error, List<String> message) {
-        if(statusCode.is4xxClientError()) {
 
-        }
-        //Inestable
-    }
 }
--- src/test/java/com/capgeticket/ventaEntradas/VentaEntradasApplicationTests.java
+++ src/test/java/com/capgeticket/ventaEntradas/VentaEntradasApplicationTests.java
@@ -1,13 +1,121 @@
 package com.capgeticket.ventaEntradas;
 
+import com.capgeticket.ventaEntradas.controller.VentaEntradasController;
+import com.capgeticket.ventaEntradas.dto.EventoDto;
+import com.capgeticket.ventaEntradas.dto.VentaEntradasDto;
+import com.capgeticket.ventaEntradas.exception.EventoNotFoundException;
+import com.capgeticket.ventaEntradas.feignClients.BancoFeignClient;
+import com.capgeticket.ventaEntradas.feignClients.EventoFeignClient;
+import com.capgeticket.ventaEntradas.repository.VentaEntradasRepository;
+import com.capgeticket.ventaEntradas.service.VentaEntradasService;
+import com.capgeticket.ventaEntradas.service.VentaEntradasServiceImpl;
+import feign.FeignException;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.springdoc.core.service.GenericResponseService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.web.servlet.MockMvc;
 
-@SpringBootTest
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.Mockito.when;
+
+@WebMvcTest(VentaEntradasController.class)
 class VentaEntradasApplicationTests {
 
-	@Test
-	void contextLoads() {
+	@Autowired
+	private MockMvc mockMvc;
+
+	@MockBean
+	private VentaEntradasService service;
+
+	@Autowired
+	private VentaEntradasServiceImpl serviceImpl;
+
+	@MockBean
+	private VentaEntradasRepository repository;
+
+	@Mock
+	private BancoFeignClient bancoFeignClient;
+
+	@Mock
+	private EventoFeignClient eventoFeignClient;
+
+	@BeforeEach
+	void setUp() {
+		MockitoAnnotations.openMocks(this);
 	}
+
+	@Test
+	void testValidCompra() {
+
+	}
+
+	@Test
+	void testInvalidCompra() {
+
+	}
+
+	@Test
+	void testValidCompraWithNonExistenEvento() {
+		VentaEntradasDto venta = new VentaEntradasDto();
+		EventoDto e = new EventoDto();
+		e.setId(1000L);
+		venta.setEvento(e);
+		when(eventoFeignClient.getEventoById(1000L)).thenThrow(FeignException.errorStatus("localhost:8081/evento/1000",null));
+		assertThrows(EventoNotFoundException.class, () -> serviceImpl.compra(venta));
+	}
+
+	@Test
+	void testValidCompraWithBankError400NoFunds() {
+
+	}
+
+	@Test
+	void testValidCompraWithBankError400NoClient() {
+
+	}
+
+	@Test
+	void testValidCompraWithBankError400InvalidCard() {
+
+	}
+
+	@Test
+	void testValidCompraWithBankError400InvalidCCV() {
+
+	}
+
+	@Test
+	void testValidCompraWithBankError400InvalidMonth() {
+
+	}
+
+	@Test
+	void testValidCompraWithBankError400InvalidYear() {
+
+	}
+
+	@Test
+	void testValidCompraWithBankError400InvalidDate() {
+
+	}
+
+	@Test
+	void testValidCompraWithBankError400InvalidName() {
+
+	}
+
+	@Test
+	void testValidCompraWithBankError500() {
+
+	}
+
+
 
 }
